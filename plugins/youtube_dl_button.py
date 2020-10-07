e4f25b0b29511feb2d0fca3d806b1a170c511a7b
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2020-10-08 00:33:11 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -271,7 +271,7 @@ async def youtube_dl_call_back(bot, update):
                     chat_id=update.message.chat.id,
                     document=download_directory,
                     thumb=thumb_image_path,
-                    caption=f"<b>{description}\n\nShare and Support\n\n@SerialCoIn</b>",
+                    caption=f"<b>description\n\nShare and Support\n\n@SerialCoIn</b>",
                     parse_mode="HTML",
                     # reply_markup=reply_markup,
                     reply_to_message_id=update.message.reply_to_message.message_id,
@@ -301,7 +301,7 @@ async def youtube_dl_call_back(bot, update):
                 await bot.send_video(
                     chat_id=update.message.chat.id,
                     video=download_directory,
-                    caption=f"<b>{description}\n\nShare and Support\n\n@SerialCoIn</b>",
+                    caption=f"<b>description\n\nShare and Support\n\n@SerialCoIn</b>",
                     parse_mode="HTML",
                     duration=duration,
                     width=width,
