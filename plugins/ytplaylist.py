8204542d3b401155d0cddeb513e58d07f889ab66
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2020-11-13 01:50:25 +0530

    Update ytplaylist.py

--- plugins/ytplaylist.py
+++ plugins/ytplaylist.py
@@ -1,24 +1,40 @@
-# (c) gautamajay52
-# 
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Shrimadhav U K
+
 # the logging things
 import logging
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-)
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-LOGGER = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
-
+import asyncio
+import json
+import math
 import os
 import shutil
-import asyncio
-import subprocess
-import requests
-from helper_funcs.upload_to_tg import upload_to_tg, upload_to_gdrive
-from tobrot import (
-    DOWNLOAD_LOCATION
-)
+import time
+from datetime import datetime
+
+# the secret configuration specific things
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+
+# the Strings used for this "thing"
+from translation import Translation
+
+import pyrogram
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+from helper_funcs.chat_base import TRChatBase
+from helper_funcs.display_progress import progress_for_pyrogram, humanbytes
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
+# https://stackoverflow.com/a/37631799/4723940
+from PIL import Image
+from helper_funcs.help_Nekmo_ffmpeg import generate_screen_shots
 
 
 async def yt_playlist_downg(message, i_m_sefg, G_DRIVE):
