1513d917a3bbf252bc6956e232e57d040237d265
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2020-11-13 02:52:45 +0530

    Update youtube_dl_extractor.py

--- plugins/youtube_dl_extractor.py
+++ plugins/youtube_dl_extractor.py
@@ -19,7 +19,7 @@ import os
 import pyrogram.types as pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-from sample_config import config (
+from sample_config import (
     DEF_THUMB_NAIL_VID_S
 )
 
