9254136c3a6f5e3418f1c8fa8affc97abbb9b22f
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2020-11-13 02:23:15 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -11,6 +11,7 @@ class Config(object):
     # Get these values from my.telegram.org
     # Array to store users who are authorized to use the bot
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_CHANNEL = set(int(x) for x in os.environ.get("AUTH_CHANNEL", "").split())
     # Banned Unwanted Members..
     BANNED_USERS = []
     # the download location, where the HTTP Server runs
