71a02bece5a31c1b8b0185cb7e1e045fb7a093e0
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2020-11-14 13:53:01 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -200,7 +200,7 @@ async def youtube_dl_call_back(bot, update):
                 is_w_f,
                 Config.DEF_WATER_MARK_FILE,
                 300,
-                9
+                1
             )
             logger.info(images)
             await bot.edit_message_text(
